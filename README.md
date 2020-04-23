1d7afba14092f4cde69576848bbaa57b7c353656
zhenik <nikita.zhevnitskiy@gmail.com>
2020-04-23 11:25:43 +0200

    update docker

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hadoop, Hive, Metastore, S3, Postgres
+Practising with big data stack
+
+## Setup
---
+++ docker/init-hive-db.sh
@@ -0,0 +1,15 @@
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE USER hive WITH PASSWORD 'hive';
+  CREATE DATABASE metastore;
+  GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
+  \c metastore
+  \pset tuples_only
+  \o /tmp/grant-privs
+SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO hive ;'
+FROM pg_tables
+WHERE tableowner = CURRENT_USER and schemaname = 'public';
+  \o
+  \i /tmp/grant-privs
+EOSQL
